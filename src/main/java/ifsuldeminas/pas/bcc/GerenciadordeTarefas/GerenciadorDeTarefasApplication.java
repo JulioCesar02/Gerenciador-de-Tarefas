ea0a727a3e4c2ce7c25b660ca8eab8d2d7492f38
Julio Cesar Giglio Tubino Junior <107716267+JulioCesar02@users.noreply.github.com>
2023-12-03 20:04:05 -0300

    update
    ultimas mudanças do projeto

--- src/main/java/ifsuldeminas/pas/bcc/GerenciadordeTarefas/GerenciadorDeTarefasApplication.java
+++ src/main/java/ifsuldeminas/pas/bcc/GerenciadordeTarefas/GerenciadorDeTarefasApplication.java
@@ -3,14 +3,24 @@ package ifsuldeminas.pas.bcc.GerenciadordeTarefas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "ifsuldeminas.pas.bcc.GerenciadordeTarefas")
 public class GerenciadorDeTarefasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GerenciadorDeTarefasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorDeTarefasApplication.class, args);
+    }
 
+    @RestController
+    public static class StaticController {
+        @GetMapping("/")
+        public String index() {
+            return "index";
+        }
+    }
 }
+
 
---
+++ src/main/java/ifsuldeminas/pas/bcc/GerenciadordeTarefas/JwtTokenProvider.java
@@ -0,0 +1,38 @@
+package ifsuldeminas.pas.bcc.GerenciadordeTarefas;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.token.expire-length}")
+    private long validityInMilliseconds;
+
+    private final Key key = Keys.hmacShaKeyFor(secretKey.getBytes());
+
+    public String generateToken(Usuario usuario) {
+        Claims claims = Jwts.claims().setSubject(usuario.getEmail());
+        claims.put("id", usuario.getId());
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
